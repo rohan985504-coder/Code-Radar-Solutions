b126f5e7c669a31d5ccf6333548f9a9cf44be0e0
rohan985504-coder <rohan0698.becse24@chitkara.edu.in>
2025-01-29 11:53:04 +0530

    Solution to Logical OR Operation

--- Logical OR Operation@@67882a731b4392d8fc1577dd/code.c
+++ Logical OR Operation@@67882a731b4392d8fc1577dd/code.c
@@ -1,12 +1,8 @@
 #include <stdio.h>
 
-char* welcome() {
-    return "Welcome to Code Radar!";
-}
-
 int main() {
     int m,n;
-    scanf("%d","%d",&m, &n);
+    scanf("%d""%d",&m, &n);
     if(m||n>0){
         printf("True");
     }
