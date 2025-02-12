f298210b34aae6230358466737c8bf19ad9c4188
rohan985504-coder <rohan0698.becse24@chitkara.edu.in>
2025-02-13 01:11:33 +0530

    Solution to Check Character Type

--- Check Character Type@@6788dd63490637cfc44e1b95/code.c
+++ Check Character Type@@6788dd63490637cfc44e1b95/code.c
@@ -6,15 +6,5 @@ int main() {
     if(a == 'a' || a =='e' || a =='i' || a =='o' || a =='u'){
     printf("Vowel");
     }
-    else if(a==2 || a==3 || a==5 || a==7){
-        printf("Prime");
-    }
-    else if(a%2==0 || a%3==0 || a%5==0 a%7==0){
-        printf("Not Prime");
-    }
-    else{
-        printf("Prime");
-    }
-
     return 0;
 }
